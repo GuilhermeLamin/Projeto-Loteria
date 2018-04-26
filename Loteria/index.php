ffdab5a56eaad3e2f112a0729b03460b44de2a26
GuilhermeLamin <32649314+GuilhermeLamin@users.noreply.github.com>
2018-04-26 11:52:08 -0300

    Add files via upload

---
+++ Loteria/index.php
@@ -0,0 +1,23 @@
+<?php 
+
+    include("cabecalho.php");
+
+?>    
+
+
+   <label for="Escolher" class="escolha2">Escolha aqui seu jogo:</label>
+		<select name="Escolher" class="escolha1">
+			
+			<option>Mega-sena</option>
+			<option>Quina</option>
+			<option>Quadra</option>
+			<option>Lotofácil</option>
+
+		</select>
+
+   <section>
+		<input type="submit" name="Prosseguir" value="Prosseguir" class="Prosseguir">
+   </section>		
+
+</body>
+</html>
---
+++ Loteria/sorteio.php
@@ -0,0 +1,10 @@
+<?php
+
+   include("cabecalho.php");
+
+?>
+      <label for="apostas">Número de apostas</label>
+      <input type="number" name="apostas">
+      
+      <label for="dezenas">Número de dezenas</label>
+      <input type="number" name="dezenas">
